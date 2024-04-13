92245586b7c4b88b308c071ed6e36c401ac3908e
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-04-13 17:11:53 +0530

    using gradio without langchain

---
+++ chatbot.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import gradio as gr
+from gradio import Textbox, Interface
+
+
+url = "http://localhost:11434/api/generate"
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+conversation_history = []
+
+
+def generate_response(prompt):
+    conversation_history.append(prompt)
+
+    full_prompt = "\n".join(conversation_history)
+
+    data = {
+        "model": "gemma:7b-instruct-q2_K",
+        "stream": False,
+        "prompt": full_prompt,
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        response_text = response.text
+        data = json.loads(response_text)
+        actual_response = data["response"]
+        conversation_history.append(actual_response)
+        return actual_response
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
+
+
+face = Interface(
+    fn=generate_response,
+    inputs=Textbox(
+        lines=2, placeholder="Enter your prompt here...", show_label=False),
+    outputs="text",
+    title="Gemma-7b",
+    description="Write a message!",
+)
+
+face.launch()
