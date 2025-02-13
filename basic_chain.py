3bd7aff134bb47b4e485338cc148e06eaf4860f1
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2025-02-13 21:40:55 +0530

    add: ollama-gemma-7b

--- basic_chain.py
+++ basic_chain.py
@@ -1,8 +1,8 @@
+from langchain.chains import LLMChain
 from langchain_community.llms import Ollama
+from langchain.prompts import PromptTemplate
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
 
 llm = Ollama(model="gemma:7b-instruct-q2_K",
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]), temperature=0.8,)
@@ -11,7 +11,6 @@ prompt = PromptTemplate(
     input_variables = ["topic"],
     template = "Tell me about this topic {topic}"
 )
-
 
 chain = LLMChain(llm=llm, prompt=prompt)
 print(chain.invoke("India"))
