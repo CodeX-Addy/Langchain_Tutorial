e3d5da315e78bbf93dcead27367187e188a7fb24
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2025-12-31 23:54:17 +0530

    add: basic chain

--- basic_chain.py
+++ basic_chain.py
@@ -7,6 +7,7 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 llm = Ollama(model="gemma:7b-instruct-q2_K",
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]), temperature=0.8,)
 
+## Basic Q&A Chain
 prompt = PromptTemplate(
     input_variables = ["topic"],
     template = "Tell me about this topic {topic}"
