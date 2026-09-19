147cad8bc5ae2ed1cb0469fc90b4593100efd6b4
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-09-19 19:38:49 +0530

    add: langchain first

---
+++ langchain_1.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+prompt = ChatPromptTemplate.from_template("Explain this {topic} in one {word_cnt} words..")
+chain = prompt | model | StrOutputParser()
+
+result = chain.invoke({"topic": "gradient descent", "word_cnt" : 20})
+print(result)
